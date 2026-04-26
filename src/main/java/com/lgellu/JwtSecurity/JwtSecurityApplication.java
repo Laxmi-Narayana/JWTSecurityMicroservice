46a0a332f80962001be872a495e0bdb985f91a08
LaxmiNarayana Gellu <laxminarayanagellu@gmail.com>
2026-04-26 09:45:36 +0530

    change of ddl-auto to update

--- src/main/java/com/lgellu/JwtSecurity/JwtSecurityApplication.java
+++ src/main/java/com/lgellu/JwtSecurity/JwtSecurityApplication.java
@@ -5,11 +5,13 @@ import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableConfigurationProperties(JwtProperties.class)
 @EnableScheduling
+@EnableDiscoveryClient
 public class JwtSecurityApplication {
 
     public static void main(String[] args) {
